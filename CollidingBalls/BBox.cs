2b84ec5efa9dae2e9a8a93d88abd73519b8cf924
Jakub Kucharzewski <jakuch.job@gmail.com>
2025-04-13 17:44:27 +0200

    Dodaj pliki projektów.

---
+++ CollidingBalls/BBox.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollidingBalls
+{
+    public class BBox
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        private List<Ball> balls = new List<Ball>();
+
+
+        public List<Ball> Balls
+        {
+            get { return balls; }            
+        }
+
+
+        public BBox(int x, int y, int w, int h)
+        {
+            Width = w;
+            Height = h;
+            X = x;
+            Y = y;
+        }
+
+
+    }
+}
---
+++ CollidingBalls/IAlgo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollidingBalls
+{
+    public interface IAlgo
+    {
+        public void NextStep(BBox box);
+    }
+}
